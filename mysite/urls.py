03738e3588264113976bbe5ca4e30c95ee11f265
Elnura D <elnurad@Elnuras-MacBook-Air.local>
2019-05-24 22:11:14 +0200

    adding login/logout feature

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', include('myblog.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
 
 
